15bfd79be8c52000e99b1be0a14aae91272b53a0
JoseLuis <JoseLuis@Desktop-JoseLuis.mshome.net>
2024-05-31 10:34:06 -0500

    Creacion de proyecto

---
+++ src/main/java/insta/curosM4A/TareaUnidad3Application.java
@@ -0,0 +1,13 @@
+package insta.curosM4A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaUnidad3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaUnidad3Application.class, args);
+	}
+
+}
---
+++ src/test/java/insta/curosM4A/TareaUnidad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package insta.curosM4A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaUnidad3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
